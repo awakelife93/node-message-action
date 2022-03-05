8aec650acaecb14c29f6a14ce0274b0e379afe4f
hw.park <hw.park@fastfive.co.kr>
2022-03-05 16:55:15 +0900

    express next type 수정

--- src/lib/protocol/express/middleware/index.ts
+++ src/lib/protocol/express/middleware/index.ts
@@ -1,9 +1,9 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 
 const middlewareController = async (
   request: Request,
   response: Response,
-  next: Function,
+  next: NextFunction,
 ) => {
   // todo: 확장할 것이 있다면 여기에...
   next();
