5c250e84ca3d8b1cb7e04d1596ff8706981622e8
harry <awakelife93@gmail.com>
2022-06-23 00:12:05 +0900

    config flag 삼항 연산자 제거

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,7 +4,7 @@ export default {
   PORT: process.env.port ?? 5005,
   SQS_SERVER_END_POINT: process.env.SQS_SERVER_END_POINT ?? "localhost:3000",
   IS_SEND_TO_SOCKET_SUBSCRIBE:
-    process.env.IS_SEND_TO_SOCKET_SUBSCRIBE === "true" ? true : false,
+    process.env.IS_SEND_TO_SOCKET_SUBSCRIBE === "true",
   REDIS_HOST: process.env.REDIS_HOST ?? "127.0.0.1",
   REDIS_PORT: process.env.REDIS_PORT ? Number(process.env.REDIS_PORT) : 6379,
   PARAMS_SPLIT_TYPE: process.env.PARAMS_SPLIT_TYPE ?? "/",
